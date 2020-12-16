2c72ba889ec626fbf22a4341ea764307d707f616
Kurtisz Krisztián <kkurtisz@gmail.com>
2020-12-16 09:33:16 +0100

    15 part2 clear

--- 15_2.rb
+++ 15_2.rb
@@ -1,25 +1,26 @@
 #part 2
+# a harmincmilliós kérdés
 starting = [9,3,1,0,8,4]
 
 counting = {}
 starting[0...-1].each_with_index do |s, i|
-  counting["#{s}"] = i+1
+  counting[s] = i+1
 end
 
 def spoken(counting, last_num, idx)
   if counting[last_num]
-    last = ((idx) - counting[last_num]).to_s
+    last = ((idx) - counting[last_num])
     counting[last_num] = idx
   else
-    last = "0"
+    last = 0
     counting[last_num] = idx
   end
 
   last
 end
 
-round = 30000000
-last_num = starting.last.to_s
+round = 30_000_000
+last_num = starting.last
 size = starting.size
 
 while round > size
